7563187842b1422ed2c4ec7d3e2853e589ab4374
Tushar Muralidharan <u7253634@anu.edu.au>
2021-09-13 16:24:13 +1000

    Update last seen GitHub API

--- js/seen.js
+++ js/seen.js
@@ -1,6 +1,19 @@
 const seenEl = document.querySelector('#lastseen');
 
 function updateSeen() {
+    fetch("https://api.github.com/users/tusharmurali/events").then(response => response.text()).then(response => {
+        let json = JSON.parse(r);
+        if (json[0].repo) {
+            // if (json[0].repo.url)
+                // Please no XSS
+                seenEl.innerHTML = `I was last seen working on <a href="http://github.com/${json[0].repo.name}" class="d" target="_blank"
+                style="color:#949494;">${json[0].repo.name.split("/")[1]}<span class="underline" style="background-color:#949494;"></span></a>.`;
+            // else
+            //     seenEl.innerText = 'I was last seen working on ' + json[0].repo.name.split("/")[1] + '.';
+        } else {
+            seenEl.innerText = "I'm not working on anything right now."
+        }
+    }).catch(console.log);
     fetch('seen.json?nocache=' + Math.random().toString().substring(2)).then(r => r.text()).then(r => {
         let json = JSON.parse(r);
         if (json.project) {
