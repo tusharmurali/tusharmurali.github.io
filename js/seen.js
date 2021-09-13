15450f2109e6d047aa95afeaae2d584fcf4f4be2
Tushar Muralidharan <u7253634@anu.edu.au>
2021-09-13 16:41:03 +1000

    Fix seen.js bug

--- js/seen.js
+++ js/seen.js
@@ -2,7 +2,7 @@ const seenEl = document.querySelector('#lastseen');
 
 function updateSeen() {
     fetch("https://api.github.com/users/tusharmurali/events").then(response => response.text()).then(response => {
-        let json = JSON.parse(r);
+        let json = JSON.parse(response);
         if (json[0].repo) {
             seenEl.innerHTML = `I was last seen working on <a href="http://github.com/${json[0].repo.name}" class="d" target="_blank"
             style="color:#949494;">${json[0].repo.name.split("/")[1]}<span class="underline" style="background-color:#949494;"></span></a>.`;
