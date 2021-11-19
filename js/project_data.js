2d6c3f5f6530f9944bee7475640e49a05437b7b3
Tushar Muralidharan <u7253634@anu.edu.au>
2021-11-20 02:39:44 +1100

    Update images, descriptions

--- js/project_data.js
+++ js/project_data.js
@@ -2,41 +2,41 @@ const ProjectData = {
 	"bettergenesis": {
 		title: "BetterGenesis",
 		github: "https://github.com/tusharmurali/better-genesis",
-		description: "BetterGenesis is a web app used by students at my high school to track their grades with tools such as grade calculators and grade trend tracking. I facetiously named it BetterGenesis as I intended it to be an enhanced version of Genesis, the school's crude grade viewer. The site is active <a href=\"https://better-genesis.herokuapp.com/\" target=\"_blank\">here</a>. It involved creating a web server in Express fit with industry standard AES-256 encryption along with a Vue frontend."
+		description: "BetterGenesis is a web application used by hundreds of students at my high school to track their grades with grade calculators and trend tracking. I facetiously named it BetterGenesis after the school's crude grade viewing system, Genesis. The site is active <a href=\"https://better-genesis.herokuapp.com/\" target=\"_blank\">here</a>. The backend consists of an Express web server with industry standard AES-256 encryption, and the frontend is made in <a href=\"https://vuejs.org\" target=\"_blank\">Vue.js</a> with <a href=\"https://vuetifyjs.com/en/\" target=\"_blank\">Vuetify</a>."
 	},
 	"glucosity": {
 		title: "Glucosity",
 		github: "",
-		description: "My goal with Glucosity is to have an online dashboard for diabetics to obtain smart predictions about their blood glucose levels by logging their carbohydrate intake and physical activity. Inspired by <a href=\"https://loopkit.github.io/loopdocs/\" target=\"_blank\">LoopKit</a>, Glucosity is a work in progress which I am working on alongside my schoolwork."
+		description: "Glucosity is a web application inspired by <a href=\"https://loopkit.github.io/loopdocs/\" target=\"_blank\">LoopKit</a> for diabetics to obtain smart predictions about their blood glucose levels by logging their carbohydrate intake and physical activity. Glucosity is a work in progress."
 	},
 	"rhythm": {
 		title: "Rhythm",
-		github: "https://github.com/tusharmurali/rhythm",
-		description: "My first venture into Java, Rhythm is a game I built for my AP Computer Science A final project. Notes fall in time to each track and the player must tap the corresponding keys. I used the AWT for visuals and JLayer for audio. <div align=\"center\"><iframe width=\"500\" height=\"250\" src=\"https://www.youtube.com/embed/8fgNbsc0y0Y\" title=\"YouTube video player\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe></div>"
+		github: "",
+		description: "Rhythm is a game I built for my AP Computer Science A final project, one of my first ventures into Java. Notes fall in time to each track, and the player must tap the corresponding keys. I used the AWT for visuals and JLayer for audio. Check out the GitHub <a href=\"https://github.com/tusharmurali/rhythm\" target=\"_blank\">here</a>.<div align=\"center\"><iframe width=\"500\" height=\"250\" src=\"https://www.youtube.com/embed/8fgNbsc0y0Y\" title=\"YouTube video player\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe></div>"
 	},
 	"harvestable": {
 		title: "Harvestable",
 		github: "https://github.com/MugPand/Harvestable",
-		description: "Harvestable is an Android app that evaluates whether it's a good day for farmers to plant or harvest crops. I created this app with <a href=\"https://github.com/MugPand\" target=\"_blank\">Mugdhesh Pandkar</a> for hackMHS V. We hope that such an app could eventually be put into production to make the hard work of agriculture more efficient. We utilized multiple climate APIs along with a research-based algorithm to forecast good harvests."
+		description: "Harvestable is an Android app that evaluates whether it's a good day for farmers to plant or harvest crops. I created this app with <a href=\"https://github.com/MugPand\" target=\"_blank\">Mugdhesh Pandkar</a> for hackMHS V. We hope that such an app could eventually be used to make the hard work of agriculture more efficient. The app utilizes multiple climate APIs along with a research-based algorithm to forecast good harvests."
 	},
 	"securcity": {
 		title: "Securcity",
 		github: "https://github.com/mattr-crooks/HackJA2018",
-		description: "Securcity is a web app that builds a complex portfolio of information about any city in the United States. I created this website with <a href=\"https://github.com/mattr-crooks\" target=\"_blank\">Matt Crooks</a>, <a href=\"https://github.com/MugPand\" target=\"_blank\">Mugdhesh Pandkar</a>, and <a href=\"https://github.com/papermango\" target=\"_blank\">Parth Deshmukh</a>, and we ended up taking the first prize in HackJA 2018. We intend for the website to be used by people who are relocating and want to gauge the safety of a new area. I worked on the backend portion of the project and utilized several APIs and data collection services to deliver a brief summary of statistics."
+		description: "Securcity is a web app that builds a complex portfolio of information about any city in the United States. I created this website with <a href=\"https://github.com/mattr-crooks\" target=\"_blank\">Matt Crooks</a>, <a href=\"https://github.com/MugPand\" target=\"_blank\">Mugdhesh Pandkar</a>, and <a href=\"https://github.com/papermango\" target=\"_blank\">Parth Deshmukh</a>, and we ended up taking first place in HackJA 2018. We intend for the website to be used by people who are relocating and want to gauge the safety of a new area. I worked on the backend portion of the project and utilized several APIs and data collection services to deliver a brief summary of statistics."
 	},
 	"melat8": {
 		title: "Melat-8&reg;",
 		github: "",
-		description: "Melat-8 is a website I build as an unpaid intern for <a href=\"https://www.pharmvista.com\" target=\"_blank\">Pharmvista</a>. The site, which is active <a href=\"http://melat8.com\" target=\"_blank\">here</a>, features their line of organic melatonin gummies in a theme similar to Apple's website. I built this project using <a href=\"https://getbootstrap.com\">Bootstrap</a> along with a healthy dose of teamwork, communication, and meeting deadlines."
+		description: "Melat-8 is a website I build as an unpaid intern for <a href=\"https://www.pharmvista.com\" target=\"_blank\">Pharmvista</a>. The site, which is active <a href=\"http://melat8.com\" target=\"_blank\">here</a>, features their line of organic melatonin gummies in a theme similar to the <a href=\"https://apple.com\" target=\"_blank\">Apple</a> website. I built this project using <a href=\"https://getbootstrap.com\">Bootstrap</a> and a healthy dose of teamwork, communication, and meeting deadlines."
 	},
 	"chessmemory": {
 		title: "ChessMemory",
 		github: "https://github.com/tusharmurali/chess-memory",
-		description: "ChessMemory is a visual memory trainer that uses Chess puzzles. Inspired by Human Benchmark's <a href=\"https://humanbenchmark.com/tests/memory\" target=\"_blank\">Visual Memory Test</a>, ChessMemory trains both visual memory and chess tactics at the same time. ChessMemory uses over 380,000 puzzles in the 1250-1750 rating range from the <a href=\"https://lichess.org\" target=\"_blank\">lichess.org</a> puzzle database. Check out the site <a href=\"https://tusharmurali.github.io/chess-memory/\" target=\"_blank\">here.</a>"
+		description: "ChessMemory is a visual memory trainer that uses Chess puzzles. Inspired by Human Benchmark's <a href=\"https://humanbenchmark.com/tests/memory\" target=\"_blank\">Visual Memory Test</a>, ChessMemory trains both visual memory and chess tactics at the same time. ChessMemory uses over 380,000 puzzles in the 1250-1750 rating range from the <a href=\"https://lichess.org\" target=\"_blank\">lichess.org</a> puzzle database. The website is also featured on <a href=\"https://database.lichess.org/#related-projects\" target=\"_blank\">https://database.lichess.org/#related-projects</a>. Check out the site <a href=\"https://tusharmurali.github.io/chess-memory/\" target=\"_blank\">here.</a>"
 	},
 	"cstimerrp": {
 		title: "csTimer Rich Presence",
 		github: "https://github.com/tusharmurali/cstimer-rp",
-		description: "csTimer Rich Presence is a small program that provides Discord Integration for the popular Rubik's Cube timer <a href=\"https://cstimer.net\">csTimer.net</a>. The program consists of some JQuery code which runs through a Chrome extension and a Python program that uses the discord.py API. Updating every 5 seconds, the program shows the average of your past 5 and 12 times as well as the event and scramble type.",
+		description: "csTimer Rich Presence is a small program that provides Discord Integration for the popular Rubik's Cube timer <a href=\"https://cstimer.net\">csTimer.net</a>. The program consists of <a href=\"https://jquery.com\" target=\"_blank\">jQuery</a> code which runs through a Chrome extension and a Python program that uses <a href=\"https://discordpy.readthedocs.io/en/stable/\" target=\"_blank\">discord.py</a>. Updating every 5 seconds, the program shows the average of the user's past 5 and 12 solve times as well as the event and scramble type.",
 	}
 };
